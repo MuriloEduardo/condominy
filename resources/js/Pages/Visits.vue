22496ce3623885139c05f03fff04fd48d5fbd354
Murilo Eduardo dos Santos <murilo.santos@somosphi.com>
2021-12-17 16:21:01 -0300

    Add frontend improves

---
+++ resources/js/Pages/Visits.vue
@@ -0,0 +1,42 @@
+<template>
+	<form>
+		<div class="d-flex">
+			<label for="name">Nome</label>
+			<input type="text" id="name" name="name" />
+		</div>
+		<div>
+			<label for="document">Documento</label>
+			<input type="text" id="document" name="document" />
+		</div>
+		<div>
+			<label for="vehicle_plate">Placa Veiculo</label>
+			<input type="text" id="vehicle_plate" name="vehicle_plate" />
+		</div>
+		<div>
+			<label for="destination_apartment">Apartamento Destino</label>
+			<input type="text" id="destination_apartment" name="destination_apartment" />
+		</div>
+		<br />
+		<button type="submit">Salvar</button>
+	</form>
+</template>
+
+<style scoped></style>
+
+<script>
+import { defineComponent } from 'vue';
+import { Link } from '@inertiajs/inertia-vue3';
+
+export default defineComponent({
+	components: {
+		Link,
+	},
+
+	props: {
+		canLogin: Boolean,
+		canRegister: Boolean,
+		laravelVersion: String,
+		phpVersion: String,
+	},
+});
+</script>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,13 +11,19 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js').vue()
-    .postCss('resources/css/app.css', 'public/css', [
-        require('postcss-import'),
-        require('tailwindcss'),
-    ])
-    .webpackConfig(require('./webpack.config'));
+mix.js('resources/js/app.js', 'public/js')
+	.vue()
+	.postCss('resources/css/app.css', 'public/css', [
+		require('postcss-import'),
+		require('tailwindcss'),
+	])
+	.webpackConfig(require('./webpack.config'))
+	.sourceMaps();
 
 if (mix.inProduction()) {
-    mix.version();
+	mix.version();
 }
+
+mix.browserSync({
+	proxy: 'http://localhost:8000',
+});
