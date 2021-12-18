346bfc429d6711ac9c99de0567b889933b251c0e
Murilo Eduardo dos Santos <murilo.santos@somosphi.com>
2021-12-18 20:24:39 -0300

    Put togetter again hamburger buttons

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -269,6 +269,18 @@
 						>
 							Dashboard
 						</jet-responsive-nav-link>
+						<jet-responsive-nav-link
+							:href="route('visits.index')"
+							:active="route().current('visits.index')"
+						>
+							Lista de Visitas
+						</jet-responsive-nav-link>
+						<jet-responsive-nav-link
+							:href="route('visits.create')"
+							:active="route().current('visits.create')"
+						>
+							Cadastros de Visitas
+						</jet-responsive-nav-link>
 					</div>
 
 					<!-- Responsive Settings Options -->
