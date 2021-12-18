7e7a2add0d2235c4b7d7a8a2b2464b7dda296df5
Murilo Eduardo dos Santos <murilo.santos@somosphi.com>
2021-12-18 15:56:31 -0300

    adjusts table full width

--- resources/js/Pages/Visit/Index.vue
+++ resources/js/Pages/Visit/Index.vue
@@ -5,7 +5,7 @@
 		</template>
 
 		<div class="max-w-7xl mx-auto sm:py-12 sm:px-6 lg:px-8 overflow-x-auto">
-			<table class="m-4">
+			<table class="m-4 w-full">
 				<thead>
 					<tr class="text-left">
 						<th>Nome</th>
